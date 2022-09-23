8bd3dc77e4d3834a60405f769370247ca170b441
Greymaster <rytbait101204@gmail.com>
2022-09-23 12:01:47 +0300

    django project update

--- MySiteProject/MySiteProject/urls.py
+++ MySiteProject/MySiteProject/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from MyInfoSite import views
 
 urlpatterns = [
+    path('myinfo/', include('MyInfoSite.urls')),
+    path('about/', views.about),
     path('admin/', admin.site.urls),
 ]
